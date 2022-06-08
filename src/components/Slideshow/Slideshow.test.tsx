3105579401cb7502249175228af781c150614b91
EricWvgg <eric@whiskyvangoghgo.com>
2022-06-08 15:43:16 +0000

    renewed snapshots

--- src/components/Slideshow/Slideshow.test.tsx
+++ src/components/Slideshow/Slideshow.test.tsx
@@ -2,8 +2,9 @@ import renderer from 'react-test-renderer'
 import Slideshow from './'
 
 describe('Slideshow', () => {
+  const sampleSlides = ['/images/sample-art-1.jpg', '/images/sample-art-2.jpg', '/images/sample-art-3.jpg']
   it('renders correctly', () => {
-    const tree = renderer.create(<Slideshow />).toJSON()
+    const tree = renderer.create(<Slideshow slides={sampleSlides} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
