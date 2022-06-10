0f6d487e1b41188056d8dc5c685570a29ebbb2b5
EricWvgg <eric@whiskyvangoghgo.com>
2022-06-10 14:11:51 +0000

    index has query

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -2,9 +2,7 @@ import { useMemo } from 'react'
 import { ApolloClient, HttpLink, HttpOptions, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
 import merge from 'deepmerge'
 import isEqual from 'lodash/isEqual'
-import { assert, isClient, isServer } from '@lib'
-
-export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
+import { assert, isServer } from '@lib'
 
 let apolloClient: ApolloClient<NormalizedCacheObject>
 
@@ -22,21 +20,21 @@ function createApolloClient(user?: ArtizenUser) {
   }
 
   return new ApolloClient({
-    ssrMode: typeof window === 'undefined',
+    ssrMode: isServer(),
     link: new HttpLink(httpOptions),
     cache: new InMemoryCache({
       typePolicies: {
         Query: {
           fields: {
+            Donations: {
+              keyArgs: false,
+            },
             Project: {
-              // Don't cache separate results based on
-              // any of this field's arguments.
+              // Don't cache separate results based on any of this field's arguments.
               // keyArgs: ['where'],
               keyArgs: false,
-              // Concatenate the incoming list items with
-              // the existing list items.
+              // Concatenate the incoming list items with the existing list items.
               merge(incoming, existing = []) {
-                // return [...existing, ...incoming]
                 return [
                   ...existing,
                   ...incoming.filter((incomingVar: any) =>
@@ -46,20 +44,12 @@ function createApolloClient(user?: ArtizenUser) {
               },
             },
             User: {
-              // Don't cache separate results based on
-              // any of this field's arguments.
+              // Don't cache separate results based on any of this field's arguments.
               // keyArgs: ['where'],
               keyArgs: ['where'],
-              // Concatenate the incoming list items with
-              // the existing list items.
+              // Concatenate the incoming list items with the existing list items.
               merge(incoming, existing = []) {
                 return [...existing, ...incoming]
-                // return [
-                //   ...existing,
-                //   ...incoming.filter((d) =>
-                //     existing.every((s) => !isEqual(d, s))
-                //   ),
-                // ]
               },
             },
           },
@@ -71,8 +61,7 @@ function createApolloClient(user?: ArtizenUser) {
 
 export function initializeApollo(initialState?: any, user?: ArtizenUser): ApolloClient<NormalizedCacheObject> {
   const newApolloClient = createApolloClient(user)
-  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
-  // Gets hydrated here
+  // If your page has Next.js data fetching methods that use Apollo Client, the initial state gets hydrated here.
   if (initialState) {
     // Get existing cache, loaded during client side data fetching
     const existingCache = newApolloClient.extract()
@@ -90,7 +79,7 @@ export function initializeApollo(initialState?: any, user?: ArtizenUser): Apollo
     newApolloClient.cache.restore(data)
   }
   // For SSG and SSR always create a new Apollo Client
-  if (typeof window === 'undefined') {
+  if (isServer()) {
     return newApolloClient
   }
   // Create the Apollo Client once in the client
@@ -102,19 +91,13 @@ export function initializeApollo(initialState?: any, user?: ArtizenUser): Apollo
 
 export function addApolloState(client: ApolloClient<NormalizedCacheObject>, pageProps: any) {
   if (pageProps?.props) {
-    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract()
+    pageProps.props['apolloData'] = client.cache.extract()
   }
   return pageProps
 }
 
 export function useApollo(pageProps: any, user?: ArtizenUser) {
-  // Const [token, setToken] = useState({loading: true, token: null, error: null})
-
-  console.log('user   ', user)
-  // Const state = pageProps[APOLLO_STATE_PROP_NAME]
-  // Const store = useMemo(() => !loading && initializeApollo(state, user), [loading || state])
-  const state = pageProps[APOLLO_STATE_PROP_NAME]
-  // Const store = useMemo(() =>  initializeApollo(state, user), [user, state])
+  const state = pageProps['apolloData']
   const store = useMemo(() => initializeApollo(state, user), [user, state])
   return { apolloClient: store, loading: Boolean(!user) }
 }
