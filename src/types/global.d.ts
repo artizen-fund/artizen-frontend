09c3b4d6a5c5abbb98ff311991af75be3b2c0a94
EricWvgg <eric@whiskyvangoghgo.com>
2022-06-14 17:20:31 +0000

    restored global file (ArtizenUser)

---
+++ src/types/global.d.ts
@@ -0,0 +1,15 @@
+import type { MagicUserMetadata } from 'magic-sdk'
+
+export {}
+
+declare global {
+  interface ArtizenUser extends MagicUserMetadata {
+    token?: string
+    id?: string
+  }
+
+  interface SimpleComponentProps {
+    children?: React.ReadNode
+    className?: string
+  }
+}
