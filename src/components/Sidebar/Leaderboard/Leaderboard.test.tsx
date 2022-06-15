cad3b9fd00c1fd5d88603b7cefc1f8e6341f614e
EricWvgg <eric@whiskyvangoghgo.com>
2022-06-15 15:05:56 -0400

    snapshot tests

--- src/components/Sidebar/Leaderboard/Leaderboard.test.tsx
+++ src/components/Sidebar/Leaderboard/Leaderboard.test.tsx
@@ -1,15 +1,13 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
-import Leaderboard from './'
+import Leaderboard, { ILeaderboard } from './'
 
 describe('Leaderboard', () => {
   it('renders Leaderboard unchanged', () => {
-    const leaderboard = [
-      { name: 'herp derp', amount: 69 },
-      { name: 'dorp donk', amount: 68 },
-      { name: 'hoop doop', amount: 67 },
-    ]
-    const { container } = render(<Leaderboard {...{ leaderboard }} />)
+    const props: ILeaderboard = {
+      Donations: [],
+    }
+    const { container } = render(<Leaderboard {...props} />)
     expect(container).toMatchSnapshot()
   })
 })
