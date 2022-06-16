0106f8979fe6bfc5b0aaf5264f614f87dd9ea70d
EricWvgg <eric@whiskyvangoghgo.com>
2022-06-16 15:26:44 +0000

    new form template

--- tools/generate.js
+++ tools/generate.js
@@ -2,12 +2,24 @@ const { generateTemplateFiles } = require('generate-template-files')
 
 const config = require('../package.json')
 
+alert('todo before merge: fill in Test and Stories with default data')
+
 generateTemplateFiles([
   {
-    option: 'React Component',
+    option: 'Generic Component',
     defaultCase: '(pascalCase)',
     entry: { folderPath: './tools/templates/component/' },
-    stringReplacers: [{ question: 'Component name (ex.SomeCoolComponent)', slot: '__name__' }],
+    stringReplacers: [{ question: 'Component name (ex.SomethingCool)', slot: '__name__' }],
+    output: {
+      path: './src/components/__name__(pascalCase)',
+      pathAndFileNameDefaultCase: '(pascalCase)',
+    },
+  },
+  {
+    option: 'Form Component',
+    defaultCase: '(pascalCase)',
+    entry: { folderPath: './tools/templates/formComponent/' },
+    stringReplacers: [{ question: 'Component name (ex.SomeForm)', slot: '__name__' }],
     output: {
       path: './src/components/__name__(pascalCase)',
       pathAndFileNameDefaultCase: '(pascalCase)',
