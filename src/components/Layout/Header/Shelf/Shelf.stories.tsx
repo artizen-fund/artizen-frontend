de4168c2cc0cd5c5d192e7e797f3aaad0cf044f9
EricWvgg <eric@whiskyvangoghgo.com>
2022-06-16 17:08:50 +0000

    new shelf component

---
+++ src/components/Layout/Header/Shelf/Shelf.stories.tsx
@@ -0,0 +1,9 @@
+import Shelf from './'
+
+export default {
+  title: 'components/Shelf',
+  component: Shelf,
+  argTypes: {},
+}
+
+export const ShelfComponent = (props: any) => <Shelf {...props} />
