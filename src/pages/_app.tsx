a1e21daf975084e696aa1fcc226cb49600e74475
rubelux <rubelux@gmail.com>
2022-06-22 11:54:58 +0100

    checking PRO env var

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,7 +44,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 }
 
 // eslint-disable-next-line
-console.log("process.env.NEXT_PUBLIC_PRO=====    ", process.env.NEXT_PUBLIC_PRO, "--typeof--", typeof process.env.NEXT_PUBLIC_PRO )
+console.log("process.env.NEXT_PUBLIC_PRO=====    ", process.env.NEXT_PUBLIC_PROD, "--typeof--", typeof process.env.NEXT_PUBLIC_PROD )
 
 const devApp = dynamic(() => Promise.resolve(withAuth()(App)), { ssr: false })
 
