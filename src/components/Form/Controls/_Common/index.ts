9843add93e01ff33861949b99da8173b6f92d022
EricWvgg <eric@whiskyvangoghgo.com>
2022-06-23 02:42:32 +0000

    renamed InputIcon component

--- src/components/Form/Controls/_Common/index.ts
+++ src/components/Form/Controls/_Common/index.ts
@@ -1,5 +1,5 @@
 export { default as InputLabel } from './InputLabel'
-export { default as InputIcon } from './InputIcon'
+export { default as InputGlyph } from './InputGlyph'
 export { default as InputWrapper } from './InputWrapper'
 export { default as Message } from './Message'
 export { default as Wrapper } from './Wrapper'
