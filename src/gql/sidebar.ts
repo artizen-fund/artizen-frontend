356898475ec0672f918788c9fe0e997ef80c5f64
rodrigopavezi <rodrigopavezi@gmail.com>
2022-06-28 15:59:26 -0300

    fix: query issues

--- src/gql/sidebar.ts
+++ src/gql/sidebar.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 export const SIDEBAR_DONATORS = gql`
   query SidebarDonators {
-    exampleEntities(orderBy: totalDonations, first: 3, orderDirection: desc) {
+    exampleEntities: exampleEntities(orderBy: donation_amount, first: 3, orderDirection: desc) {
       donation_amount
       donation_donor
       totalDonations
