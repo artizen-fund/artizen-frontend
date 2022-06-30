4d58ff2f1ef957ebea9fd26d4f87acce55c8294d
EricWvgg <eric@whiskyvangoghgo.com>
2022-06-30 13:38:33 +0000

    moved session detection out of shelf

--- src/components/Layout/Header/SessionShelf/index.tsx
+++ src/components/Layout/Header/SessionShelf/index.tsx
@@ -1,17 +1,9 @@
-import { useEffect } from 'react'
 import { useSession } from '@lib'
 import LoginShelf from '../LoginShelf'
 import AccountShelf from '../AccountShelf'
 
 const SessionShelf = () => {
-  const { user, checkSession } = useSession()
-
-  console.log('user  ', user)
-
-  useEffect(() => {
-    checkSession()
-  }, [])
-
+  const { user } = useSession()
   return !!user ? <AccountShelf {...{ user }} /> : <LoginShelf />
 }
 
