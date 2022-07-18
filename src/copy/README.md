db1186d65f2046f56bd92867c1e76fbd1a8f9a72
EricWvgg <eric@whiskyvangoghgo.com>
2022-07-18 18:09:24 +0000

    new juju assets

---
+++ src/copy/README.md
@@ -0,0 +1,7 @@
+# Note
+
+Ideally this copy would come from a CMS like Strapi, Sanity, or Contentful.
+
+But we're not quite there yet!
+
+To help keep the codebase clean and neutral, we're putting the copy here as a stopgap.
---
+++ src/copy/home/metrics.ts
@@ -0,0 +1,28 @@
+const metrics: Array<{
+  image: string
+  amount: number
+  description: string
+}> = [
+  {
+    description: 'total raised',
+    image: '/assets/illustrations/metrics/total.svg',
+    amount: 1515250,
+  },
+  {
+    description: 'total raised',
+    image: '/assets/illustrations/metrics/this-month.svg',
+    amount: 15250,
+  },
+  {
+    description: 'total raised',
+    image: '/assets/illustrations/metrics/average.svg',
+    amount: 150,
+  },
+  {
+    description: 'total raised',
+    image: '/assets/illustrations/metrics/members.svg',
+    amount: 8212,
+  },
+]
+
+export { metrics }
