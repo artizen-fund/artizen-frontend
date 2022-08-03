c06907d4b5f0f0a9eab263cbb19af1a7e31d8c95
EricWvgg <eric@whiskyvangoghgo.com>
2022-08-03 17:44:08 +0000

    fixed login form, optimized intercom lib

--- src/lib/Intercom.ts
+++ src/lib/Intercom.ts
@@ -4,42 +4,44 @@ import { loadIntercom, trackEvent, shutdownIntercom } from 'next-intercom'
 
 export const notifications = []
 
-export const intercomEventEnum: { [key: string]: string } = {
-  USER_LOGIN: 'user:login',
-  DONATION_FIAT_START: 'donation:fiat:start',
-  DONATION_FIAT_TOPUP: 'donation:fiat:pay:topup',
-  DONATION_FIAT_CONFIRMED: 'donation:fiat:confirmed',
-  DONATION_CRYTO_START: 'donation:crypto:start',
-  DONATION_CRYTO_SWAP: 'donation:crypto:swap',
-  DONATION_CRYTO_BRIDGE: 'donation:crypto:bridge',
-  DONATION_CRYTO_CONFIRMED: 'donation:crypto:confirmed',
+export enum intercomEventEnum {
+  USER_LOGIN = 'user:login',
+  DONATION_FIAT_START = 'donation:fiat:start',
+  DONATION_FIAT_TOPUP = 'donation:fiat:pay:topup',
+  DONATION_FIAT_CONFIRMED = 'donation:fiat:confirmed',
+  DONATION_CRYTO_START = 'donation:crypto:start',
+  DONATION_CRYTO_SWAP = 'donation:crypto:swap',
+  DONATION_CRYTO_BRIDGE = 'donation:crypto:bridge',
+  DONATION_CRYTO_CONFIRMED = 'donation:crypto:confirmed',
 }
 
-export const trackEventF = (type: string, target: object = {}) => {
-  if (type !== intercomEventEnum[type]) {
-    new Error('Unknown Event label')
-  }
-  trackEvent(type, target)
-}
+export const trackEventF = (type: intercomEventEnum, target: object = {}) => trackEvent(type, target)
 
 export function initIntercom() {
   const [loggedInUser] = useLoggedInUser()
 
-  const NEXT_PUBLIC_INTERCOM_APP_ID = assert(process.env.NEXT_PUBLIC_INTERCOM_APP_ID, 'NEXT_PUBLIC_INTERCOM_APP_ID')
+  const appId = assert(process.env.NEXT_PUBLIC_INTERCOM_APP_ID, 'NEXT_PUBLIC_INTERCOM_APP_ID')
 
   const startIntercom = useCallback(() => {
     loadIntercom({
-      appId: NEXT_PUBLIC_INTERCOM_APP_ID,
-      email: loggedInUser?.email, //default: ''
+      appId,
+      email: loggedInUser?.email, // default: ''
       name: loggedInUser && `${loggedInUser?.firstName} ${loggedInUser?.lastName}`,
       ssr: false, // default: false
       initWindow: true, // default: true
-      delay: 0, // default: 0  - usefull for mobile devices to prevent blocking the main thread
+      delay: 0, // default: 0  - useful for mobile devices to prevent blocking the main thread
     })
   }, [loggedInUser])
 
   useEffect(() => {
-    startIntercom()
+    if (!!loggedInUser) {
+      startIntercom()
+    } else {
+      shutdownIntercom()
+    }
+  }, [loggedInUser])
+
+  useEffect(() => {
     return () => shutdownIntercom()
   }, [])
 }
