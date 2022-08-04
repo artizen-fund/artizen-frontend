a1bc95c5dc47ac17ebd8884e6b35d26cff82aa4a
rodrigopavezi <rodrigopavezi@gmail.com>
2022-08-04 09:38:58 -0300

    fix per the PR

--- src/components/Donations/ProcessCrypto/index.tsx
+++ src/components/Donations/ProcessCrypto/index.tsx
@@ -22,7 +22,7 @@ const ProcessCrypto = ({ setStage, donationMethod }: IProcessCrypto) => {
         setCryptoStage('building')
       }
     })
-  })
+  }, [])
 
   return (
     <Wrapper>
--- src/lib/Notification.tsx
+++ src/lib/Notification.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { CourierProvider } from '@trycourier/react-provider'
 import { useLoggedInUser } from './useLoggedInUser'
+import { assert } from '@lib'
 
 type INotification = {
   children: JSX.Element
@@ -8,11 +9,13 @@ type INotification = {
 
 export const Notification = ({ children }: INotification) => {
   const [loggedInUser] = useLoggedInUser()
-
   return (
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    <CourierProvider userId={`${loggedInUser?.id}`} clientKey={process.env.NEXT_PUBLIC_COURIER_CLIENT_KEY}>
+    <CourierProvider
+      userId={`${loggedInUser?.id}`}
+      clientKey={assert(process.env.NEXT_PUBLIC_COURIER_CLIENT_KEY, 'NEXT_PUBLIC_COURIER_CLIENT_KEY')}
+    >
       {children}
     </CourierProvider>
   )
