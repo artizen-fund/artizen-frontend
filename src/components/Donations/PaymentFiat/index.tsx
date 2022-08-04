e6d5a883b0572067dfad1fc9f907ea3f882c9730
EricWvgg <eric@whiskyvangoghgo.com>
2022-08-04 20:49:49 +0000

    restored form

--- src/components/Donations/PaymentFiat/index.tsx
+++ src/components/Donations/PaymentFiat/index.tsx
@@ -1,8 +1,8 @@
-import { useMemo, useState, useEffect } from 'react'
+import { useMemo, useState } from 'react'
 import styled from 'styled-components'
 import { useReactiveVar } from '@apollo/client'
 import { Button, DonationHelpLink, Form, CheckboxControl } from '@components'
-import { payWithFiat, userMetadataVar, useLoggedInUser, calculateFee } from '@lib'
+import { payWithFiat, userMetadataVar, useLoggedInUser } from '@lib'
 import { breakpoint } from '@theme'
 import { schema, uischema, initialState, FormState } from '@forms/paymentFiat'
 
@@ -11,15 +11,15 @@ interface IPaymentFiat {
   amount: number
 }
 
-type FormStage = 'gatheringPersonal' | 'gatheringPayment' | 'processing'
+const TRANSACTION_FEE = 42
 
 const PaymentFiat = ({ setStage, amount }: IPaymentFiat) => {
   const [loggedInUser] = useLoggedInUser()
-  const [formStage, setFormStage] = useState<FormStage>('gatheringPersonal')
-
   const LOCALSTORAGE_KEY = 'fiatPayment'
   const [savePaymentInfo, setSavePaymentInfo] = useState(false)
+  // TODO: ^ where/how is this stored?
   const [paymentData, setPaymentData] = useState<FormState>(initialState)
+  const [processing, setProcessing] = useState(false)
 
   useMemo(() => {
     if (typeof localStorage === 'undefined') {
@@ -36,34 +36,22 @@ const PaymentFiat = ({ setStage, amount }: IPaymentFiat) => {
 
   const metadata = useReactiveVar(userMetadataVar)
 
+  // amount, metadata, paymentData, loggedInUser
   const processTransaction = async () => {
     if (!loggedInUser) {
       return
     }
-    setFormStage('processing')
+    setProcessing(true)
     try {
       await payWithFiat(amount, paymentData, loggedInUser, metadata)
       setStage('processCrypto')
     } catch {
-      setFormStage('gatheringPersonal')
+      setProcessing(false)
     }
   }
 
-  const [transactionFee, setTransactionFee] = useState<number>()
-  useEffect(() => {
-    if (!paymentData.country) return
-    setTransactionFee(calculateFee(amount, paymentData.country as string))
-  }, [paymentData])
-
-  const [disabled, setDisabled] = useState(true)
-  useEffect(() => {
-    console.log('paymentData', paymentData, uischema)
-    const supportedRegion = (paymentData.country, paymentData.state)
-    setDisabled(formStage === 'processing' || !transactionFee || !supportedRegion)
-  }, [formStage, transactionFee, paymentData])
-
   return (
-    <Wrapper className={formStage}>
+    <Wrapper className={processing ? 'processing' : ''}>
       <Information>
         <div>
           <Title>Let’s enter your payment information</Title>
@@ -74,8 +62,8 @@ const PaymentFiat = ({ setStage, amount }: IPaymentFiat) => {
           <p>Donation Summary</p>
           <ul>
             <li>Donation: ${amount}</li>
-            <li>Transaction fee: {!!transactionFee ? 'checking' : `$${transactionFee}`}</li>
-            <li>Purchase total: {!!transactionFee ? 'checking' : `$${amount + transactionFee!}`}</li>
+            <li>Transaction fee: ${TRANSACTION_FEE}</li>
+            <li>Purchase total: ${amount + TRANSACTION_FEE}</li>
           </ul>
         </div>
 
@@ -89,15 +77,12 @@ const PaymentFiat = ({ setStage, amount }: IPaymentFiat) => {
       <Form
         localStorageKey={LOCALSTORAGE_KEY}
         {...{ schema, uischema, initialState }}
-        readonly={formStage === 'processing'}
+        readonly={processing}
         data={paymentData}
         setData={setPaymentData}
       >
-        <GatherPaymentButton stretch onClick={() => setFormStage('gatheringPayment')}>
-          Payment Info
-        </GatherPaymentButton>
-        <SubmitButton stretch onClick={processTransaction} {...{ disabled }}>
-          Transfer donation
+        <SubmitButton stretch onClick={processTransaction}>
+          Transfer ${amount + TRANSACTION_FEE}
         </SubmitButton>
         <ProcessingMessage>hum de dooo</ProcessingMessage>
       </Form>
@@ -116,11 +101,7 @@ const Information = styled.div`
 const Title = styled.h1``
 
 const SubmitButton = styled(props => <Button {...props} />)`
-  grid-area: submitButton;
-`
-
-const GatherPaymentButton = styled(props => <Button {...props} />)`
-  grid-area: gatherPaymentButton;
+  grid-area: submit;
 `
 
 const ProcessingMessage = styled.div`
@@ -133,35 +114,35 @@ const Wrapper = styled.div`
   gap: 10px;
   grid-template-areas:
     'copy copy'
-    'street street'
-    'city city'
-    'state country'
+    'first_name last_name'
+    'number number'
+    'month year'
+    'zip verification_value'
     'phone_number phone_number'
-    'gatherPaymentButton gatherPaymentButton';
-  &.gatheringPayment {
+    'submit submit';
+  &.submitted {
     grid-template-areas:
-      'copy copy'
-      'first_name last_name'
-      'cc_number cc_number'
-      'month year'
-      'zip verification_value'
-      'submitButton submitButton';
+      'copy'
+      'optIn'
+      'confirmation';
   }
   @media only screen and (min-width: ${breakpoint.laptop}px) {
     gap: 12px;
     grid-template-areas:
-      'copy copy street street'
-      'copy copy city city'
-      'copy copy state country'
+      'copy copy first_name last_name'
+      'copy copy number number'
+      'copy copy month year'
+      'copy copy zip verification_value'
       'copy copy phone_number phone_number'
-      'copy copy gatherPaymentButton gatherPaymentButton';
-    &.gatheringPayment {
+      'copy copy submit submit';
+    &.submitted {
       grid-template-areas:
-        'copy copy first_name last_name'
-        'copy copy cc_number cc_number'
-        'copy copy month year'
-        'copy copy zip verification_value'
-        'copy copy submitButton submitButton';
+        'copy copy processing processing'
+        'copy copy processing processing'
+        'copy copy processing processing'
+        'copy copy processing processing'
+        'copy copy processing processing'
+        'copy copy processing processing';
     }
   }
   @media only screen and (min-width: ${breakpoint.desktop}px) {
@@ -182,7 +163,7 @@ const Wrapper = styled.div`
   }
 
   *[id='#/properties/number'] {
-    grid-area: cc_number;
+    grid-area: number;
   }
 
   *[id='#/properties/verification_value'] {
@@ -206,6 +187,9 @@ const Wrapper = styled.div`
   }
 
   &.processing {
+    *[id='#/properties/EMAIL'],
+    *[id='#/properties/FNAME'],
+    *[id='#/properties/LNAME'],
     ${SubmitButton} {
       display: none;
     }
