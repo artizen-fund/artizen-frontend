cab340f457685d77580f5413df6c42b8bb682c80
EricWvgg <eric@whiskyvangoghgo.com>
2022-09-01 12:26:24 -0400

    localstorage fix

--- src/lib/auth/AuthForm.tsx
+++ src/lib/auth/AuthForm.tsx
@@ -2,18 +2,19 @@ import { useMemo, useState } from 'react'
 import { useRouter } from 'next/router'
 import { Form, Button, PagePadding } from '@components'
 import { schema, uischema, initialState, FormState } from './form'
+import { useFormLocalStorage } from '@lib'
 
 export const AuthForm = () => {
   const router = useRouter()
-
-  const [data, setData] = useState<FormState>(initialState)
+  const localStorageKey = 'ARTIZEN_DEV_PASSWORD'
+  const [data, setData] = useFormLocalStorage<FormState>(localStorageKey, initialState)
 
   const submit = () => router.reload()
 
   return (
     <PagePadding>
       <h1>Please enter the Artizen Frontend Preview password.</h1>
-      <Form {...{ schema, uischema, initialState, data, setData }} />
+      <Form {...{ schema, uischema, initialState, data, setData, localStorageKey }} />
       <Button onClick={() => submit()} level={1}>
         Submit
       </Button>
