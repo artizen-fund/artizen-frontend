f87e5ddc0753591cfeb79c7453d90bee082a6fb1
rubelux <rubelux@gmail.com>
2022-09-14 12:07:30 +0100

    added version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import '@public/styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   // eslint-disable-next-line
-  console.log('--- version: 0.1.1 ----')
+  console.log('--- version: 0.1.2 ----')
   const apolloClient = initializeApollo(pageProps?.apolloData || {})
 
   return (
