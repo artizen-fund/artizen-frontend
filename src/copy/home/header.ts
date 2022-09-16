5ce9946c64e67ce86c6dfc20c600b4eb34fb1199
EricWvgg <eric@whiskyvangoghgo.com>
2022-09-16 21:22:19 +0000

    home copy

--- src/copy/home/header.ts
+++ src/copy/home/header.ts
@@ -1,7 +1,7 @@
 const header = {
   title: 'We’ve Successfully Raised $1,515,250 For Climate Action',
   subtitle:
-    'Join us in building the world’s largest web3 fund for public goods. 100% transparent, easy to take part in, and profitable for everyone.',
+    'Each month, we award unrestricted grants to creators at the intersection of art, tech, and design. Apply for funding or donate to help select the winning projects and earn an ownership stake in the Artizen Fund.',
 }
 
 export { header }
