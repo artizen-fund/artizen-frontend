8467f6af9d61a1aaea9d324b7a243a885af02264
EricWvgg <eric@whiskyvangoghgo.com>
2022-09-23 20:01:24 +0000

    account button enhancements

--- src/components/Layout/Header/AccountButton/index.tsx
+++ src/components/Layout/Header/AccountButton/index.tsx
@@ -1,42 +1,53 @@
-import { useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useApolloClient } from '@apollo/client'
 import styled from 'styled-components'
 import { Glyph } from '@components'
-import { breakpoint, palette } from '@theme'
-import { rgba, refreshSession, useLoggedInUser, isClient } from '@lib'
+import { breakpoint, palette, typography } from '@theme'
+import { Maybe } from '@types'
+import { rgba, refreshSession, useLoggedInUser, isClient, DonationContext } from '@lib'
 
 const AccountButton = (props: SimpleComponentProps) => {
   const apolloClient = useApolloClient()
   const [loggedInUser] = useLoggedInUser()
+  const { visibleShelf } = useContext(DonationContext)
 
   useEffect(() => {
     if (isClient()) refreshSession(apolloClient)
   }, [])
 
-  const TEMP_INITIALS = 'RP'
-
-  // TODO: this component has two states, loggedIn: true or false
-  // There's actually three…
-  // SessionShelf visible "close X"
-  // SessionShelf hidden and logged out "sign in"
-  // SessionShelf hidden and logged in "avatar or initials view"
+  const [avatarDisplay, setAvatarDisplay] = useState<'avatar' | 'initials' | 'placeholder' | undefined>()
+  useEffect(() => {
+    setAvatarDisplay(
+      !loggedInUser
+        ? undefined
+        : !!loggedInUser.profileImage
+        ? 'avatar'
+        : !!loggedInUser.firstName && !!loggedInUser.lastName
+        ? 'initials'
+        : 'placeholder',
+    )
+  }, [loggedInUser])
 
   return (
-    <Wrapper loggedIn={!!loggedInUser} {...props}>
-      <SignInLabel loggedIn={!!loggedInUser} />
-      <HamburgerGlyph loggedIn={!!loggedInUser} color="night" darkColor="moon" glyph="hamburger" />
-      <AvatarImage loggedIn={!!loggedInUser}>
-        <Initials>{TEMP_INITIALS}</Initials>
-      </AvatarImage>
+    <Wrapper loggedIn={!!loggedInUser} visibleShelf={!!visibleShelf} {...props}>
+      <CloseLabel visible={!!visibleShelf}>Close</CloseLabel>
+      <SignInLabel visible={!loggedInUser && !visibleShelf}>Sign In</SignInLabel>
+      <HamburgerGlyph visible={!!loggedInUser && !visibleShelf} color="night" darkColor="moon" glyph="hamburger" />
+      <AvatarWrapper active={!!loggedInUser && !visibleShelf}>
+        <AvatarImage active={avatarDisplay === 'avatar'} profileImage={loggedInUser?.profileImage} />
+        <AvatarImage active={avatarDisplay === 'placeholder'}>
+          <Glyph glyph="face" level={1} color="night" darkColor="moon" />
+        </AvatarImage>
+        <Initials active={avatarDisplay === 'initials'}>
+          {loggedInUser?.firstName?.substring(0, 1)}
+          {loggedInUser?.lastName?.substring(0, 1)}
+        </Initials>
+      </AvatarWrapper>
     </Wrapper>
   )
 }
 
-interface LoginState {
-  loggedIn: boolean
-}
-
-const Wrapper = styled.div<LoginState>`
+const Wrapper = styled.div<{ loggedIn: boolean; visibleShelf: boolean }>`
   position: relative;
 
   display: flex;
@@ -45,16 +56,16 @@ const Wrapper = styled.div<LoginState>`
   align-items: center;
 
   padding: 4px;
-  width: ${props => (props.loggedIn ? 72 : 85)}px;
+  width: ${props => (!props.loggedIn && props.visibleShelf ? 95 : props.loggedIn ? 72 : 85)}px;
   height: 40px;
   @media only screen and (min-width: ${breakpoint.laptop}px) {
     padding: 6px;
-    width: ${props => (props.loggedIn ? 84 : 99)}px;
+    width: ${props => (!props.loggedIn && props.visibleShelf ? 114 : props.loggedIn ? 84 : 99)}px;
     height: 48px;
   }
   @media only screen and (min-width: ${breakpoint.desktop}px) {
     padding: 8px;
-    width: ${props => (props.loggedIn ? 96 : 119)}px;
+    width: ${props => (!props.loggedIn && props.visibleShelf ? 141 : props.loggedIn ? 96 : 119)}px;
     height: 56px;
   }
 
@@ -69,7 +80,7 @@ const Wrapper = styled.div<LoginState>`
   transition: width 0.3s ease-in-out;
 `
 
-const SignInLabel = styled.div<LoginState>`
+const SignInLabel = styled.div<{ visible: boolean }>`
   position: absolute;
   z-index: 1;
   top: 0;
@@ -82,19 +93,41 @@ const SignInLabel = styled.div<LoginState>`
   justify-content: center;
   align-items: center;
 
-  &:after {
-    content: 'Sign in';
-    font-style: normal;
-    font-size: 13px;
-    line-height: 100%;
-  }
+  ${typography.label.l1}
 
-  opacity: ${props => (props.loggedIn ? 0 : 1)};
+  opacity: ${props => (props.visible ? 1 : 0)};
   transition: opacity 0.15s ease-in-out;
   pointer-events: none;
 `
 
-const HamburgerGlyph = styled(props => <Glyph {...props} />)<LoginState>`
+const CloseLabel = styled.div<{ visible: boolean }>`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  ${typography.label.l1}
+  padding-top: 2px;
+  @media only screen and (min-width: ${breakpoint.laptop}px) {
+    padding-top: 2.5px;
+  }
+  @media only screen and (min-width: ${breakpoint.desktop}px) {
+    padding-top: 3.5px;
+  }
+
+  opacity: ${props => (props.visible ? 1 : 0)};
+  transition: opacity 0.15s ease-in-out;
+  pointer-events: none;
+`
+
+const HamburgerGlyph = styled(props => <Glyph {...props} />)<{ visible: boolean }>`
   width: 32px;
   @media only screen and (min-width: ${breakpoint.laptop}px) {
     width: 36px;
@@ -103,12 +136,31 @@ const HamburgerGlyph = styled(props => <Glyph {...props} />)<LoginState>`
     width: 40px;
   }
 
-  opacity: ${props => (props.loggedIn ? 1 : 0)};
+  opacity: ${props => (props.visible ? 1 : 0)};
   transition: opacity 0.3s ease-in-out;
 `
 
-const AvatarImage = styled.div<LoginState>`
-  display: flex;
+const AvatarWrapper = styled.div<{ active: boolean }>`
+  position: relative;
+  width: 32px;
+  height: 32px;
+  @media only screen and (min-width: ${breakpoint.laptop}px) {
+    width: 36px;
+    height: 36px;
+  }
+  @media only screen and (min-width: ${breakpoint.desktop}px) {
+    width: 40px;
+    height: 40px;
+  }
+  opacity: ${props => (props.active ? 1 : 0)};
+  transform: scale(${props => (props.active ? 1 : 0)});
+  transition: opacity 0.3s ease-in-out, transform 0.5s ease-in-out;
+`
+
+const AvatarImage = styled.div<{ active: boolean; profileImage?: Maybe<string> }>`
+  position: absolute;
+  display: ${props => (props.active ? 'flex' : 'none')};
+  justify-content: center;
   align-items: center;
   border-radius: 9999px;
 
@@ -127,21 +179,35 @@ const AvatarImage = styled.div<LoginState>`
 
   color: ${rgba(palette.night)};
   background-color: ${rgba(palette.stone)};
+  background-image: ${props => props.profileImage};
   @media (prefers-color-scheme: dark) {
     color: ${rgba(palette.moon)};
     background-color: ${rgba(palette.barracuda)};
   }
-
-  opacity: ${props => (props.loggedIn ? 1 : 0)};
-  transform: scale(${props => (props.loggedIn ? 1 : 0)});
-  transition: opacity 0.3s ease-in-out, transform 0.5s ease-in-out;
 `
 
-const Initials = styled.div`
-  width: 100%;
+const Initials = styled.div<{ active: boolean }>`
+  position: absolute;
+  display: ${props => (props.active ? 'flex' : 'none')};
+  justify-content: center;
+  align-items: center;
+  border-radius: 9999px;
 
-  font-style: normal;
+  width: 32px;
+  height: 32px;
+  @media only screen and (min-width: ${breakpoint.laptop}px) {
+    width: 36px;
+    height: 36px;
+  }
+  @media only screen and (min-width: ${breakpoint.desktop}px) {
+    width: 40px;
+    height: 40px;
+  }
+
   text-align: center;
+
+  color: ${rgba(palette.night)};
+  background-color: ${rgba(palette.stone)};
 
   font-size: 13px;
   @media only screen and (min-width: ${breakpoint.laptop}px) {
