840fe92d77bbd94ed9aa2aaf742a6a6730f4772a
EricWvgg <eric@whiskyvangoghgo.com>
2022-10-04 22:39:55 +0000

    form cleanup

--- src/components/Layout/Header/SessionShelf/SignupShelf/index.tsx
+++ src/components/Layout/Header/SessionShelf/SignupShelf/index.tsx
@@ -140,9 +140,11 @@ const Wrapper = styled.div`
     grid-template-areas:
       'copy firstName lastName'
       'copy email email'
+      'copy . .'
       'tocCheck submit submit';
     &.submitted {
       grid-template-areas:
+        'copy confirmation confirmation'
         'copy confirmation confirmation'
         'copy confirmation confirmation'
         'tocCheck confirmation confirmation';
