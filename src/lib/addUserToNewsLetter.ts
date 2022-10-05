3d623ba4b4f6a46d662316276f70e626eb4cb863
rubelux <rubelux@gmail.com>
2022-10-05 15:02:11 +0100

    added asset to check env variables

--- src/lib/addUserToNewsLetter.ts
+++ src/lib/addUserToNewsLetter.ts
@@ -1,10 +1,15 @@
 // const mailchimp = require('@mailchimp/mailchimp_marketing')
 import { setConfig, lists } from '@mailchimp/mailchimp_marketing'
 import md5 from 'md5'
+import { assert } from '@lib'
+
+const mailChimpApiKey = assert(process.env.MAILCHIMP_API_KEY, 'MAILCHIMP_API_KEY')
+const mailChimpPrefix = assert(process.env.MAILCHIMP_PREFIX, 'MAILCHIMP_PREFIX')
+const mailChimpAudicienId = assert(process.env.MAILCHIMP_AUDIENCE_ID, 'MAILCHIMP_AUDIENCE_ID')
 
 setConfig({
-  apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_PREFIX,
+  apiKey: mailChimpApiKey,
+  server: mailChimpPrefix,
 })
 
 export const addUserToNewsLetter = async (mail: string | null) => {
@@ -16,7 +21,7 @@ export const addUserToNewsLetter = async (mail: string | null) => {
   let response
   try {
     const subcriberHash = md5(mail.toLocaleLowerCase())
-    response = await lists.setListMember(process.env.MAILCHIMP_AUDIENCE_ID || '', subcriberHash, {
+    response = await lists.setListMember(mailChimpAudicienId || '', subcriberHash, {
       email_address: mail,
       status_if_new: 'subscribed',
       status: 'subscribed',
