d9e98096fffa88232caea38936a2dcf758f34367
rodrigopavezi <rodrigopavezi@gmail.com>
2022-10-07 11:04:51 -0300

    fix for USDC Donation and sendwyre api calls

--- src/pages/api/onramp/authorization.ts
+++ src/pages/api/onramp/authorization.ts
@@ -4,9 +4,12 @@ import { assert } from '@lib'
 
 const authorizationHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const SENDWYRE_SECRET = assert(process.env.SENDWYRE_SECRET, 'SENDWYRE_SECRET')
+  const SENDRYRE_BASE_URL = assert(process.env.NEXT_PUBLIC_SENDWYRE_BASE_URL, 'NEXT_PUBLIC_SENDWYRE_BASE_URL')
   const sdk = api('@wyre-hub/v4#fyktdr28l3w9dqt1')
   // todo: what's that? ^ why is it hard coded?
   sdk.auth(SENDWYRE_SECRET)
+  sdk.server(SENDRYRE_BASE_URL)
+
   if (!req.cookies.token) return res.status(401).json({ message: 'User is not logged in' })
   try {
     const authorization = await sdk.GetAuthorization({ orderId: req.query.orderId })
