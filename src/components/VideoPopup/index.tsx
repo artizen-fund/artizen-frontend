3ace6d5f3bd0e6721f8a9ae7cc706a8013b674ed
EricWvgg <eric@whiskyvangoghgo.com>
2022-10-11 18:58:32 +0000

    tap-to-play video

---
+++ src/components/VideoPopup/index.tsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { CloseButton } from '@components'
+import { rgba } from '@lib'
+import { palette } from '@theme'
+
+interface IVideoPopup {
+  src?: string
+  visible: boolean
+  setVisible: (b: boolean) => void
+}
+
+const VideoPopup = ({ src, visible, setVisible }: IVideoPopup) => {
+  const [loaded, setLoaded] = useState<boolean>()
+
+  return (
+    <>
+      <Wrapper className={visible ? 'visible' : ''} onClick={() => setVisible(false)}>
+        {visible && (
+          <Content>
+            <Video
+              loop={true}
+              autoPlay={true}
+              controls={false}
+              muted={true}
+              onCanPlay={() => setLoaded(true)}
+              className={loaded ? 'visible' : ''}
+            >
+              <source src={src} type="video/mp4" />
+            </Video>
+            <CloseButton visible={loaded} />
+          </Content>
+        )}
+      </Wrapper>
+      <Onionskin className={visible ? 'visible' : ''} onClick={() => setVisible(false)} />
+    </>
+  )
+}
+
+const Wrapper = styled.div`
+  position: fixed;
+  z-index: 104;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  pointer-events: none;
+`
+
+const Content = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Video = styled.video`
+  opacity: 0;
+  transform: scale(1.1);
+  /* transition out timing */
+  transition: opacity 0.15s 0s ease-in-out, transform 0.15s 0s ease-in-out;
+  max-width: 90vmin;
+  max-height: 90vmin;
+  &.visible {
+    opacity: 1;
+    transform: scale(1);
+    /* transition in timing */
+    transition: opacity 1.5s 1s ease-in-out, transform 1.5s 1s ease-in-out;
+    pointer-events: all;
+  }
+  will-change: opacity;
+  border-radius: 16px;
+`
+
+// TODO: combine this with system-wide Onionskin
+// (requires building into LayoutContext, or whatever we call that)
+
+const Onionskin = styled.div`
+  position: fixed;
+  z-index: 103;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+
+  background: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+  transition: opacity 0.15s 0s ease-in-out;
+  pointer-events: none;
+  &.visible {
+    opacity: 1;
+    pointer-events: all;
+    transition: opacity 1.5s 0.15s ease-in-out;
+  }
+  will-change: opacity;
+`
+
+export default VideoPopup
