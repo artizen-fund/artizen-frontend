8d207a65b3aef5375dd20d2b9fec92f4ce5227a6
rubelux <rubelux@gmail.com>
2022-10-14 09:21:03 +0100

    added discord badge

--- tests/e2e/specs/login-spec.js
+++ tests/e2e/specs/login-spec.js
@@ -4,6 +4,8 @@ before(() => {
 
 describe('Start donation', () => {
   it('login', () => {
+    const randomId = Math.floor(Math.random() * 100000)
+    const userId = `user${randomId}`
     cy.visit('/')
     cy.get('input[type=password]').type('2wasdsdd')
     cy.contains('Submit').click()
@@ -11,6 +13,23 @@ describe('Start donation', () => {
     cy.contains('Donate').click()
     cy.contains('Polygon').click()
     cy.contains('Continue').click()
+    cy.get('input[placeholder="e.g. René"]').type('manolo')
+    cy.get('input[placeholder="e.g. Zellweger"]').type('manolo')
+    cy.get('input[placeholder="e.g. rene@artizen.fund"]').type(`${userId}@email.ghostinspector.com`)
+    cy.contains('Sign In / Sign Up').click()
+    cy.wait(1500)
+    cy.origin(`https://email.ghostinspector.com/${userId}`, () => {
+      // Inside callback baseUrl is https://lab.artizen.fund/
+      cy.visit('/')
+      cy.contains('Log in to Artizen_dev').click()
+      cy.wait(500)
+      cy.get('.login-button').click()
+      cy.wait(2500)
+      //   // Commands are executed in secondary origin
+      //   cy.contains("10Sep2022 No Inertia:");
+      // Passed in values are accessed via callback args
+    })
+    cy.visit('/')
     // // cy.contains("Sign").click();
     // // cy.contains("Confirm").click();
     // // cy.contains("Connect Wallet").click();
