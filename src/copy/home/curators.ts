236cf6b90643968016f77795697fbd716a0a071b
EricWvgg <eric@whiskyvangoghgo.com>
2022-10-16 13:05:16 -0400

    prettier

--- src/copy/home/curators.ts
+++ src/copy/home/curators.ts
@@ -3,8 +3,16 @@ export const curators = {
   copy: 'Meet the community leaders who review submissions and select finalists',
   curators: [
     { name: 'Asad Malik', title: 'CEO of Jadu', portrait: '/assets/illustrations/curators/Asad-J-Malik.png' },
-    { name: 'Julia Kaganskiy', title: 'Founding Director of NEW INC', portrait: '/assets/illustrations/curators/Julia-Kaganskiy.png' },
+    {
+      name: 'Julia Kaganskiy',
+      title: 'Founding Director of NEW INC',
+      portrait: '/assets/illustrations/curators/Julia-Kaganskiy.png',
+    },
     { name: 'Path', title: 'NFT Collector', portrait: '/assets/illustrations/curators/Path.png' },
-    { name: 'Elliot Cole', title: 'Composer and Technologist', portrait: '/assets/illustrations/curators/Elliot-Cole.png' },
+    {
+      name: 'Elliot Cole',
+      title: 'Composer and Technologist',
+      portrait: '/assets/illustrations/curators/Elliot-Cole.png',
+    },
   ],
 }
