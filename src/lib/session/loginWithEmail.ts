7e4db4b9e895c2bccc592a173dc29cae952acfb4
rubelux <rubelux@gmail.com>
2022-10-17 14:01:55 +0100

    removed console

--- src/lib/session/loginWithEmail.ts
+++ src/lib/session/loginWithEmail.ts
@@ -3,7 +3,6 @@ import { createSession } from '@lib'
 
 export const loginWithEmail = async (apolloClient: ApolloClient<object>, magic: MagicInstance, email: string) => {
   const didToken = await magic.auth.loginWithMagicLink({ email, showUI: false })
-  console.log('loginWithEmail  didToken   ', didToken)
   if (!didToken) throw 'Error retrieving token with email'
   createSession(apolloClient, didToken)
 }
