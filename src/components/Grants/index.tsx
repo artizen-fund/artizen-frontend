73d7782c5f8818223a7c661814d96363f0c1cc94
rodrigopavezi <rodrigopavezi@gmail.com>
2022-12-02 09:24:58 -0300

    add end and cancel grants calls

--- src/components/Grants/index.tsx
+++ src/components/Grants/index.tsx
@@ -1,10 +1,36 @@
 import { useGrant } from '@lib'
+import { useState } from 'react'
+import styled from 'styled-components'
 import Button from '../Button'
 
 export const Grants = () => {
-  const { publish } = useGrant()
+  const [grantId, setGrantId] = useState(0)
+  const { publish, endGrant, cancelGrant } = useGrant()
 
-  return <Button onClick={() => publish('')}>Publish</Button>
+  return (
+    <>
+      <Container>
+        <label>
+          Grant ID:
+          <input
+            type="text"
+            value={grantId}
+            onChange={event => setGrantId(Number(event.target.value))}
+            style={{ backgroundColor: '#FFFFFF' }}
+          />
+        </label>
+
+        <Button onClick={() => endGrant(grantId)}>End Grant</Button>
+        <Button onClick={() => cancelGrant(grantId)}>Cancel Grant</Button>
+      </Container>
+      <Button onClick={() => publish('')}>Publish</Button>
+    </>
+  )
 }
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`
 
 export default Grants
