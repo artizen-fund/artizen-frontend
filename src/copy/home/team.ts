d5a5e41cf33c83b507f82921d1679a1de2acbe36
EricWvgg <eric@whiskyvangoghgo.com>
2022-12-13 15:53:36 +0000

    a couple team fixes

--- src/copy/home/team.ts
+++ src/copy/home/team.ts
@@ -1,11 +1,10 @@
 export const team = {
   title: 'Our Team',
-  copy: 'Meet the community leaders who review submissions and select finalists',
   curators: [
     { name: 'René Pinnell', title: 'Co-Founder & CEO', portrait: '/assets/illustrations/team/rene.jpg' },
     { name: 'Ruben Campos', title: 'Co-Founder & CTO', portrait: '/assets/illustrations/team/ruben.jpg' },
     { name: 'Simon Corry', title: 'Head of Product', portrait: '/assets/illustrations/team/simon.jpg' },
-    { name: 'Dov Heichemer', title: 'Header of Community', portrait: '/assets/illustrations/team/dov.jpg' },
+    { name: 'Dov Heichemer', title: 'Head of Community', portrait: '/assets/illustrations/team/dov.jpg' },
     {
       name: 'Juju Parchimowicz',
       title: 'Product Design & Art Direction',
@@ -16,8 +15,8 @@ export const team = {
       title: 'Mathematician + Blockchain Dev',
       portrait: '/assets/illustrations/team/eric-k.jpg',
     },
-    { name: 'Zsofie Tubel', title: 'Blockchain Dev', portrait: '/assets/illustrations/team/zsofia.jpg' },
-    { name: 'Eric Jacobsen', title: 'Frontend Dev', portrait: '/assets/illustrations/team/eric-j.jpg' },
+    { name: 'Zsofie Tubel', title: 'Blockchain Developer', portrait: '/assets/illustrations/team/zsofia.jpg' },
+    { name: 'Eric Jacobsen', title: 'Frontend Developer', portrait: '/assets/illustrations/team/eric-j.jpg' },
     { name: 'Karolina Kurcwald', title: 'Writer', portrait: '/assets/illustrations/team/karolina.jpg' },
     { name: 'Ryan Juliano', title: 'Legal', portrait: '/assets/illustrations/team/ryan.jpg' },
   ],
