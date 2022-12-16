63bb00e9c74a8ac2f2512c7e2b9ee8a0ecd89184
rubelux <rubelux@gmail.com>
2022-12-16 20:02:34 +0000

    added changes

--- src/pages/admin/grants/[id].tsx
+++ src/pages/admin/grants/[id].tsx
@@ -2,14 +2,18 @@ import { useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { Layout, CreateGrants, Spinner } from '@components'
+import { assert } from '@lib'
 
 const grantDetails = () => {
   const router = useRouter()
-  const { status } = useSession()
+  const { status, data } = useSession()
 
   useEffect(() => {
-    if (status === 'unauthenticated') router.push('/')
-  }, [status])
+    const GRANT_CURATOR_ROLE = assert(process.env.NEXT_PUBLIC_GRANT_CURATOR_ROLE, 'NEXT_PUBLIC_GRANT_CURATOR_ROLE')
+    if (status === 'unauthenticated' && data !== null && (data as any).user?.id === GRANT_CURATOR_ROLE) {
+      router.push('/')
+    }
+  }, [status, data])
 
   return <Layout>{status !== 'authenticated' ? <Spinner /> : <CreateGrants />}</Layout>
 }
