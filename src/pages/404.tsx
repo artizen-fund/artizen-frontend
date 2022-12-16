6e30e194826175447bfd11d3a0a1976293ad0da0
EricWvgg <eric@whiskyvangoghgo.com>
2022-12-16 19:21:31 +0000

    goes to 404 if no grant

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,46 @@
-import { Button } from '@components'
+import styled from 'styled-components'
+import { Layout, Button } from '@components'
+import { typography } from '@theme'
 
 const Page = () => (
-  <div>
-    <p>
-      Looks like the page you&apos;re looking for doesn&apos;t exist. There might be a typo in the url address or the
-      page might have moved.
-    </p>
-    <Button href="/">Go back to the hompage</Button>
-  </div>
+  <Layout>
+    <Content>
+      <Box>
+        <h1>Now, that’s not good!</h1>
+        <p>
+          Looks like the page you’re looking for doesn’t exist. There might be a typo in the url address or the page
+          might have moved. Let’s get you to an actual existing page instead.
+        </p>
+        <Button href="/grants/today" level={1} outline>
+          Go back to the hompage
+        </Button>
+      </Box>
+    </Content>
+  </Layout>
 )
+
+const Content = styled.div`
+  min-height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+
+  padding: 20px;
+  max-width: 700px;
+  h1 {
+    ${typography.title.l2}
+  }
+  p {
+    ${typography.body.l2}
+  }
+`
 
 export default Page
