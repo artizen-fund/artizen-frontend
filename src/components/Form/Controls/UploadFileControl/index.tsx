8a87671f588b63766d31490af7e31ad8d9053d7e
EricWvgg <eric@whiskyvangoghgo.com>
2022-12-22 02:03:22 +0000

    media modal

--- src/components/Form/Controls/UploadFileControl/index.tsx
+++ src/components/Form/Controls/UploadFileControl/index.tsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import styled from 'styled-components'
-import { InvisiFileInput, uploadToCloudinary, fileType } from '@lib'
+import { Spinner } from '@components'
+import { InvisiFileInput, useCloudinary, fileType, rgba, LayoutContext, sleep } from '@lib'
 import { StringControlProps } from '../StringControl'
-import { breakpoint } from '@theme'
+import { breakpoint, palette } from '@theme'
 
 interface UploadFileControlProps {
   onBlur: () => void
@@ -29,30 +30,49 @@ const UploadFileControl = ({
     uploadFile(selectedFile)
   }, [selectedFile])
 
+  const { setVisibleModalWithAttrs } = useContext(LayoutContext)
+
+  const { upload, uploading } = useCloudinary()
   const uploadFile = async (newFile: File) => {
     if (!uischema?.options?.fileFormats.find((supportedFormat: string) => supportedFormat === newFile.type)) {
-      console.error('illegal file type')
+      alert('illegal file type')
       return
     }
-    try {
-      const cloudinaryResponse = await uploadToCloudinary(newFile)
-      const cloudinaryUrl = cloudinaryResponse.secure_url
-      handleChange!(path, cloudinaryUrl)
-    } catch (error) {
-      console.error('error uploading to cloudinary', error)
+    const cloudinaryResponse = await upload(newFile)
+    if (!cloudinaryResponse) {
+      console.error('Error uploading to Cloudinary')
     }
+    const cloudinaryUrl = cloudinaryResponse?.secure_url
+    handleChange!(path, cloudinaryUrl)
+  }
+
+  const showModal = () => {
+    if (!selectedFile || !data) return
+    setVisibleModalWithAttrs?.(
+      'media',
+      fileType(selectedFile) === 'image'
+        ? {
+            imageFile: data,
+          }
+        : {
+            videoFile: data,
+          },
+    )
   }
 
   return (
-    <InvisiFileInput setFile={setSelectedFile}>
-      <input
-        {...{ required, onBlur }}
-        disabled={!enabled || processing}
-        type="string"
-        placeholder={uischema?.options?.placeholder || ' '}
-        value={data}
-      />
-      <PreviewWrapper visible={!!selectedFile && !!data}>
+    <>
+      <InvisiFileInput setFile={setSelectedFile}>
+        <input
+          {...{ required, onBlur }}
+          disabled={!enabled || processing}
+          type="string"
+          placeholder={uischema?.options?.placeholder || ' '}
+          value={data}
+        />
+      </InvisiFileInput>
+      <PreviewWrapper visible={!!selectedFile && !!data} {...{ uploading }} filled={!!data} onClick={() => showModal()}>
+        {uploading && <StyledSpinner alwaysLight />}
         {fileType(selectedFile) === 'image' && !!data && <PreviewImage src={data} />}
         {fileType(selectedFile) === 'video' && !!data && (
           <video loop={true} autoPlay={true} controls={false} muted={true}>
@@ -60,30 +80,57 @@ const UploadFileControl = ({
           </video>
         )}
       </PreviewWrapper>
-    </InvisiFileInput>
+    </>
   )
 }
 
-const PreviewWrapper = styled.div<{ visible: boolean }>`
+const StyledSpinner = styled(props => <Spinner {...props} />)`
   position: absolute;
+  z-index: 101;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+const PreviewWrapper = styled.div<{ visible: boolean; uploading: boolean; filled: boolean }>`
+  position: absolute;
+  z-index: 100;
   top: 0;
   right: 0;
-  display: ${props => (props.visible ? 'block' : 'none')};
+  display: ${props => (props.visible || props.uploading ? 'block' : 'none')};
   height: 56px;
+  min-width: 56px;
   @media only screen and (min-width: ${breakpoint.laptop}px) {
     height: 64px;
+    min-width: 64px;
   }
   @media only screen and (min-width: ${breakpoint.desktop}px) {
     height: 72px;
+    min-width: 72px;
   }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   text-align: center;
-  background-color: #fff;
+  background-color: ${rgba(palette.moon)};
 
   appearance: none;
   outline: none;
 
   align-items: center;
-  border: 1px solid black;
+  border: 1px solid ${props => rgba(props.filled ? palette.night : palette.stone)};
+  background-image: ${props =>
+    !props.uploading && props.filled ? 'none' : 'url(/assets/glyphs/download/20/solid.svg)'};
+  background-position: center;
+  background-repeat: no-repeat;
+  cursor: pointer;
+  video {
+    max-height: 100%;
+  }
+  pointer-events: ${props => (props.filled ? 'all' : 'none')};
 `
 
 const PreviewImage = styled.img`
