9b1306b1a834df7eee90c9a908dc5b3a4bfe27b4
rubelux <rubelux@gmail.com>
2022-12-22 16:07:28 +0100

    extended NextAuth User Interface with extra value isCurator

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth, { User, Session, NextAuthOptions, AdapterUser } from 'next-auth'
+import NextAuth, { User, Session, NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import Moralis from 'moralis'
 import * as jsonwebtoken from 'jsonwebtoken'
