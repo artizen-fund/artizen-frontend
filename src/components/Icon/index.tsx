81066236c61cd434047f4ff8a818c9a432a9684b
EricWvgg <eric@whiskyvangoghgo.com>
2022-12-30 20:50:31 +0000

    Shimmer loading

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Glyph } from '@components'
+import { Glyph, Shimmer } from '@components'
 import { breakpoint, palette, Palette, GlyphKey, Level, typography } from '@theme'
 import { rgba, gapForLevel, sizeForLevel } from '@lib'
 
@@ -14,6 +14,7 @@ export interface IconProps {
   darkColor?: string
   animating?: boolean
   error?: boolean
+  loading: boolean
 }
 
 const Icon = ({
@@ -27,6 +28,7 @@ const Icon = ({
   label,
   animating,
   error,
+  loading,
   ...props
 }: IconProps) => {
   const calculatedColor =
@@ -35,21 +37,29 @@ const Icon = ({
     !outline && error ? 'uiAlert' : darkColor ? darkColor : color ? color : outline ? 'moon' : 'night'
   return (
     <Wrapper {...{ level }} {...props}>
-      <Circle {...{ outline, level, inverted }} color={calculatedColor} darkColor={calculatedDarkColor}>
-        <Glyph
-          {...{ glyph, level, animating }}
-          outline={glyphOutline}
-          color={calculatedColor}
-          darkColor={calculatedDarkColor}
-        />
-      </Circle>
-      {label && (
+      {loading ? (
+        <IconShimmer {...{ level }} />
+      ) : (
+        <Circle {...{ outline, level, inverted }} color={calculatedColor} darkColor={calculatedDarkColor}>
+          <Glyph
+            {...{ glyph, level, animating }}
+            outline={glyphOutline}
+            color={calculatedColor}
+            darkColor={calculatedDarkColor}
+          />
+        </Circle>
+      )}
+      {!!label && loading ? (
+        <Shimmer />
+      ) : !!label ? (
         <Label
           color={!outline && error ? 'uiAlert' : inverted ? 'white' : 'night'}
           darkColor={!outline && error ? 'uiAlert' : 'moon'}
         >
           {label}
         </Label>
+      ) : (
+        <></>
       )}
     </Wrapper>
   )
@@ -62,21 +72,20 @@ type CircleProps = Pick<IconProps, 'level' | 'outline' | 'inverted'> & {
 
 const Circle = styled.div<CircleProps>`
   display: flex;
+  flex-basis: 1;
   justify-content: center;
   align-items: center;
   width: ${props => sizeForLevel('mobile', props.level || 0)}px;
-  height: ${props => sizeForLevel('mobile', props.level || 0)}px;
+  height: 100%;
   @media only screen and (min-width: ${breakpoint.laptop}px) {
     width: ${props => sizeForLevel('laptop', props.level || 0)}px;
-    height: ${props => sizeForLevel('laptop', props.level || 0)}px;
   }
   @media only screen and (min-width: ${breakpoint.desktop}px) {
     width: ${props => sizeForLevel('desktop', props.level || 0)}px;
-    height: ${props => sizeForLevel('desktop', props.level || 0)}px;
   }
+  border-radius: 9999px;
 
   border: 2px solid;
-  border-radius: 9999px;
 
   border-color: ${props => rgba(props.outline ? palette[props.color] : 'transparent')};
 
@@ -90,6 +99,7 @@ const Circle = styled.div<CircleProps>`
 `
 
 const Label = styled.div<{ color: keyof Palette; darkColor: keyof Palette }>`
+  flex: 1;
   ${typography.label.l2}
   color: ${props => rgba(palette[props.color])};
   @media (prefers-color-scheme: dark) {
@@ -102,13 +112,31 @@ const Wrapper = styled.div<Pick<IconProps, 'level'>>`
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
+  width: 100%;
+  height: ${props => sizeForLevel('mobile', props.level || 0)}px;
   gap: ${props => gapForLevel('mobile', props.level)}px;
   @media only screen and (min-width: ${breakpoint.laptop}px) {
+    height: ${props => sizeForLevel('laptop', props.level || 0)}px;
     gap: ${props => gapForLevel('laptop', props.level)}px;
   }
   @media only screen and (min-width: ${breakpoint.desktop}px) {
+    height: ${props => sizeForLevel('desktop', props.level || 0)}px;
     gap: ${props => gapForLevel('desktop', props.level)}px;
   }
+`
+
+const IconShimmer = styled(props => <Shimmer {...props} />)`
+  height: 100%;
+  flex-grow: 0;
+  flex-shrink: 0;
+  flex-basis: ${props => sizeForLevel('mobile', props.level || 0)}px;
+  @media only screen and (min-width: ${breakpoint.laptop}px) {
+    flex-basis: ${props => sizeForLevel('laptop', props.level || 0)}px;
+  }
+  @media only screen and (min-width: ${breakpoint.desktop}px) {
+    flex-basis: ${props => sizeForLevel('desktop', props.level || 0)}px;
+  }
+  border-radius: 9999px;
 `
 
 export default Icon
