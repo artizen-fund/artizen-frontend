0b038ca142b79e009235379f0d7f52c3a1a49040
EricWvgg <eric@whiskyvangoghgo.com>
2023-01-05 19:50:08 +0000

    - huge form fixes - better handling for setting initialState - removed a couple deprecated components

--- src/components/SettingsParts/EditWallet/EditWallet.test.tsx
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer'
-import EditWallet from './'
-
-describe('EditWallet', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(<EditWallet />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-})
