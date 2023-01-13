5ac852aec8a77873ee6b948d91e1635d2a7699b6
EricWvgg <eric@whiskyvangoghgo.com>
2023-01-13 17:29:09 +0000

    renamed method

---
+++ src/components/CreateGrants/NewGrantForm/lib/mapArtifactsForDB.ts
@@ -0,0 +1,41 @@
+import { Artifacts } from '@forms/createGrants'
+
+export type MappedArtifact = {
+  edition: 'community' | 'patron' | 'creator'
+  artwork?: string
+  video?: string
+}
+
+export const mapArtifactsForDB = (artifactsData: Artifacts): Array<MappedArtifact> => {
+  // TODO: This should be break down into smaller units
+  // The ordering is specified here
+  // https://docs.google.com/document/d/1Jwsmc3WxCK_Z-uRCjLNhTp8EbJGSF0CT7MNDbg0jmQk/edit
+  // Creator, Community, Patron
+
+  console.log('artifactsData', artifactsData)
+
+  return [
+    {
+      edition: 'creator',
+      artwork: artifactsData.artworkCreator,
+      video: artifactsData.videoCreator,
+      // blockChainAddress: '',
+      // submissionId: '',
+      // name: '',
+      // description: '',
+      // edition: '',
+      // dateMining: '',
+      // token: '',
+    },
+    {
+      edition: 'community',
+      artwork: artifactsData.artworkCommunity,
+      video: artifactsData.videoCommunity,
+    },
+    {
+      edition: 'patron',
+      artwork: artifactsData.artworkPatron,
+      video: artifactsData.videoPatron,
+    },
+  ]
+}
