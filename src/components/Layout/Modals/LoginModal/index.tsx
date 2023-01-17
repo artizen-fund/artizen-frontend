e7ef57f7be8ecc2fdd466e3c3cf63c6bed32129a
EricWvgg <eric@whiskyvangoghgo.com>
2023-01-17 04:05:24 +0000

    removed console statement

--- src/components/Layout/Modals/LoginModal/index.tsx
+++ src/components/Layout/Modals/LoginModal/index.tsx
@@ -12,11 +12,7 @@ const LoginModal = ({ ...props }) => {
   const { connectMetamask, connectOtherWallet } = useWalletConnect()
   const { toggleModal } = useContext(LayoutContext)
 
-  console.log('window.web3', window.web3)
-
   const [enabled, setEnabled] = useState(true)
-
-  console.log('enabled  ')
 
   return (
     <Wrapper {...props}>
