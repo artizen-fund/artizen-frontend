b572ba932f73093e2ba122bc456749351251bd7f
EricWvgg <eric@whiskyvangoghgo.com>
2023-01-17 04:05:55 +0000

    hides walletConnect

--- src/components/Layout/Modals/LoginModal/index.tsx
+++ src/components/Layout/Modals/LoginModal/index.tsx
@@ -25,10 +25,12 @@ const LoginModal = ({ ...props }) => {
           Metamask
         </Tile>
 
-        <Tile onClick={() => connectOtherWallet()} {...{ enabled }}>
+        {/*
+          <Tile onClick={() => connectOtherWallet()} {...{ enabled }}>
           <img src={assetPath('/assets/walletConnect.svg')} alt="WalletConnect" />
           WalletConnect
         </Tile>
+        */}
       </Tiles>
 
       <CheckWrapper>
