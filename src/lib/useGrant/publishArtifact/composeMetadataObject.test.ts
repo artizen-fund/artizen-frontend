8742a034173818b4c888d4ff2545a34347df8881
Zsofie <zsofia@linumlabs.com>
2023-02-08 13:25:26 -0800

    added composeMetadataObject tests

--- src/lib/useGrant/publishArtifact/composeMetadataObject.test.ts
+++ src/lib/useGrant/publishArtifact/composeMetadataObject.test.ts
@@ -1,5 +1,12 @@
 import composeMetadataObject from './composeMetadataObject'
-import { mockProject, mockArtifact, mockImageResponse } from './mockData'
+import {
+  mockProject,
+  mockArtifact,
+  mockImageResponse,
+  mockAnimationResponse,
+  mockArtifactNoEdition,
+  mockProjectNoTitle,
+} from './mockData'
 import { BigNumber } from 'ethers'
 
 describe('composeMetadataObject', () => {
@@ -15,6 +22,37 @@ describe('composeMetadataObject', () => {
     )
     expect(result.name).toEqual('Test Artifact Name')
     expect(result.description).toEqual('Test artifact description goes here')
-    expect(result).toEqual(BigNumber.from('1'))
+    expect(result.image).toEqual(`ipfs://${mockImageResponse.IpfsHash}`)
+    expect(result.animation_url).toEqual(`ipfs://${mockAnimationResponse.IpfsHash}`)
+    expect(result.background_color).toEqual('000000')
+    expect(result.external_url).toEqual(`https://artizen.fund/projects/artifacts/artifact${BigNumber.from('1')}/`)
+    //TODO expect(result.attributes).toContain('Project Created')
+  })
+
+  it('reverts if edition property is missing', () => {
+    const result = composeMetadataObject(
+      'Test Artifact Name',
+      'Test artifact description goes here',
+      BigNumber.from('1'),
+      mockProject,
+      mockArtifactNoEdition,
+      1,
+      mockImageResponse,
+    )
+
+    expect(result).toThrowError('Artifact is missing “edition” property')
+  })
+  it('reverts if project title is missing', () => {
+    const result = composeMetadataObject(
+      'Test Artifact Name',
+      'Test artifact description goes here',
+      BigNumber.from('1'),
+      mockProjectNoTitle,
+      mockArtifact,
+      1,
+      mockImageResponse,
+    )
+
+    expect(result).toThrowError('Project is missing “title” property')
   })
 })
--- src/lib/useGrant/publishArtifact/mockData.ts
+++ src/lib/useGrant/publishArtifact/mockData.ts
@@ -31,6 +31,36 @@ const mockProject: IProjectFragment = {
   ],
 }
 
+const mockProjectNoTitle: IProjectFragment = {
+  __typename: 'Projects',
+  id: 'abc123',
+  impact: 'test impact string',
+  impactTags: `['tag1', 'tag2', 'tag3']`,
+  logline: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur et.',
+  description: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur et.',
+  creationDate: '2022-01-01 00:00:00',
+  completionDate: '2022-01-02 00:00:00',
+  walletAddress: '0x00000000000',
+  members: [
+    {
+      __typename: 'ProjectMembers',
+      id: 'abc234',
+      type: 'lead',
+      user: {
+        __typename: 'Users',
+        id: 'abc345',
+        firstName: 'Herp',
+        lastName: 'Derp',
+        artizenHandle: 'herpderp',
+        twitterHandle: 'derpherp',
+        website: 'https://derp.com',
+        profileImage: null,
+        publicAddress: 'null',
+      },
+    },
+  ],
+}
+
 const mockArtifact: IArtifactFragment = {
   __typename: 'Artifacts',
   id: 'abc123',
@@ -44,11 +74,34 @@ const mockArtifact: IArtifactFragment = {
   token: '0x00000000',
   createdAt: '2022-01-01 00:00:00',
 }
-
+const mockArtifactNoEdition: IArtifactFragment = {
+  __typename: 'Artifacts',
+  id: 'abc123',
+  name: 'test artifact',
+  description: 'Donec sed odio dui.',
+  artwork: 'https://artizenartifacts.com/herpderp.jpg',
+  video: null,
+  blockchainAddress: '0x0000000000',
+  dateMinting: '2022-01-01 00:00:00',
+  token: '0x00000000',
+  createdAt: '2022-01-01 00:00:00',
+}
 const mockImageResponse = {
   IpfsHash: 'QmbwaRdfkS2V3ae2DvpUnCuLQw2pTUDLQiN4DrmomTXUzq',
   PinSize: 12113,
   Timestamp: '2023-02-06T23:47:56.336Z',
 }
 
-export { mockProject, mockArtifact, mockImageResponse }
+// TODO
+const mockAnimationResponse = {
+  IpfsHash: 'QmbwaRdfkS2V3ae2DvpUnCuLQw2pTUDLQiN4DrmomTXUzq',
+}
+
+export {
+  mockProject,
+  mockArtifact,
+  mockImageResponse,
+  mockAnimationResponse,
+  mockArtifactNoEdition,
+  mockProjectNoTitle,
+}
