41ccda3cf1fbd0f642c03d1c3d24a149d78ea561
EricWvgg <eric@whiskyvangoghgo.com>
2023-02-28 18:04:06 +0000

    removed tests and storybooks

--- src/components/HomeParts/ApplyForFundingBlurb/ApplyForFundingBlurb.test.tsx
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer'
-import ApplyForFundingBlurb from './'
-
-describe('ApplyForFundingBlurb', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(<ApplyForFundingBlurb />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-})
--- src/components/Table/TableAvatar/TableAvatar.stories.tsx
+++
@@ -1,9 +0,0 @@
-import TableAvatar from './'
-
-export default {
-  title: 'components/TableAvatar',
-  component: TableAvatar,
-  argTypes: {},
-}
-
-export const TableAvatarComponent = (props: any) => <TableAvatar {...props} />
