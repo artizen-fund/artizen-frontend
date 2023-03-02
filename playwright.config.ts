d62fc30958bc44e11d3a6abd9736c5c68f0c250e
geometrizen <ek@artizen.fund>
2023-03-02 05:22:00 -0800

    adding e2e tests over recently updated dev commits

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,9 +10,10 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  globalSetup: './tests/util/globalSetup.ts',
+  testDir: './tests/e2e',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 120 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -21,7 +22,7 @@ export default defineConfig({
     timeout: 5000
   },
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -35,7 +36,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL ? process.env.PLAYWRIGHT_TEST_BASE_URL : "http://localhost:3000/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -48,15 +49,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -84,7 +85,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
+  //   command: 'yarn start',
   //   port: 3000,
   // },
 });
---
+++ tests/util/globalSetup.ts
@@ -0,0 +1,17 @@
+import dappwright, { MetaMaskWallet } from "@tenkeylabs/dappwright";
+import { FullConfig } from '@playwright/test';
+
+async function globalSetup(config: FullConfig) {
+  const [metamask, page, context] = await dappwright.bootstrap("", {
+    wallet: "metamask",
+    version: MetaMaskWallet.recommendedVersion,
+  });
+
+  console.log("process.env.PLAYWRIGHT_TEST_BASE_URL")
+  console.log(process.env.PLAYWRIGHT_TEST_BASE_URL);
+  await metamask.switchNetwork('Goerli');
+
+  await context.close();
+}
+
+export default globalSetup;
