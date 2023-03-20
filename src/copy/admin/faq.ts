8f788d6dd8fbb18e1def81cfd5b6a5a51c1b1b49
rubelux <rubelux@gmail.com>
2023-03-20 16:00:16 +0000

    updated

--- src/copy/admin/faq.ts
+++ src/copy/admin/faq.ts
@@ -1,11 +1,13 @@
 const faq = [
   {
     question: 'How to create a new season?',
-    answer: `To create a new season, you need to go to the [Seasons Page](/admin/seasons) page, check the current seasons times and click on the **Create New Season** button to create a new one.`,
+    answer:
+      'To create a new season, you need to go to the [Seasons Page](/admin/seasons) page, check the current seasons times and click on the **Create New Season** button to create a new one.',
   },
   {
     question: 'How to create a new project?',
-    answer: `To create a new project, you need to go to the [New Projects Page](/admin/projects/new) page and click on the **Create Project** button.`,
+    answer:
+      'To create a new project, you need to go to the [New Projects Page](/admin/projects/new) page and click on the **Create Project** button.',
   },
   {
     // eslint-disable-next-line
