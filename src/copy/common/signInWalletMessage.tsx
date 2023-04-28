bcd230a8340490c21187026347838843ce52f13d
rubelux <rubelux@gmail.com>
2023-04-28 13:51:37 +0100

    added signin

---
+++ src/copy/common/signInWalletMessage.tsx
@@ -0,0 +1,7 @@
+const signInWalletMessage = {
+  headline: 'Sign & Verify Message',
+  subhead: ' We take security very seriously, so you need sign the message we sent you to verify you own your wallet',
+  buttonCopy: 'Send Message',
+}
+
+export { signInWalletMessage }
