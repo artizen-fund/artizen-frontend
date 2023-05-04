58ad7e336357b6287ed1df79df68af25c606dc85
rubelux <rubelux@gmail.com>
2023-05-04 12:48:51 +0100

    update changes

--- src/components/DonationBox.tsx
+++ src/components/DonationBox.tsx
@@ -1,9 +1,13 @@
 import { useState, useContext } from 'react'
 import { useSession } from 'next-auth/react'
+import { SeasonsAbi } from '@contracts'
+import { goerli } from 'wagmi/chains'
 import styled from 'styled-components'
+import { ethers } from 'ethers'
+import { usePrepareContractWrite, useContractWrite, useBalance } from 'wagmi'
 import { ErrorObject } from 'ajv'
 import { Button, Counter } from '@components'
-import { LayoutContext, trackEventF, intercomEventEnum, assertFloat, rgba, useSeasons } from '@lib'
+import { LayoutContext, trackEventF, intercomEventEnum, assertFloat, assert, rgba, useSeasons } from '@lib'
 import { breakpoint, typography, palette } from '@theme'
 import { IProjectFragment } from '@types'
 
@@ -17,6 +21,27 @@ const DonationBox = ({ tokenId, project }: IDonationBox) => {
     process.env.NEXT_PUBLIC_BASE_ARTIFACT_PRICE,
     'NEXT_PUBLIC_BASE_ARTIFACT_PRICE',
   )
+
+  const SEASON_CONTRACT = assert(
+    process.env.NEXT_PUBLIC_SEASONS_CONTRACT_ADDRESS,
+    'NEXT_PUBLIC_SEASONS_CONTRACT_ADDRESS',
+  )
+
+  const { config } = usePrepareContractWrite({
+    address: SEASON_CONTRACT as `0x${string}`,
+    abi: SeasonsAbi,
+    functionName: 'mintArtifact',
+    args: [[130], [1]],
+    chainId: goerli.id,
+    overrides: {
+      value: ethers.utils.parseEther('0.01'),
+    },
+    onError: e => {
+      console.log('error prepering contract', e)
+    },
+  })
+
+  const { write, isLoading, isSuccess } = useContractWrite(config)
 
   const { status } = useSession()
 
@@ -36,35 +61,37 @@ const DonationBox = ({ tokenId, project }: IDonationBox) => {
       tokenId,
     })
 
-    const { error, txHash } = await mintOpenEditions(tokenId, artifactQuantity, BASE_ARTIFACT_PRICE)
+    write?.()
 
-    //All good, there is a txHash
-    if (txHash) {
-      // NOTE: This will trigger a blockchain
-      // event which is captured by event listener script (EK's owned)
-      // event listener script writes a openEditions record in Hasura
-      // which is then picked up by the subscription and the UI is updated
-      // and sends Courier email and in-app notification to the donor
+    // const { error, txHash } = await mintOpenEditions(tokenId, artifactQuantity, BASE_ARTIFACT_PRICE)
 
-      trackEventF(intercomEventEnum.DONATION_FINISHED, {
-        amount: artifactQuantity.toString(),
-        tokenId,
-      })
+    // //All good, there is a txHash
+    // if (txHash) {
+    //   // NOTE: This will trigger a blockchain
+    //   // event which is captured by event listener script (EK's owned)
+    //   // event listener script writes a openEditions record in Hasura
+    //   // which is then picked up by the subscription and the UI is updated
+    //   // and sends Courier email and in-app notification to the donor
 
-      setVisibleModalWithAttrs('shareTransaction', {
-        mode: 'postTransaction',
-        destination: `/projects/${project.titleURL}`,
-        projecTitle: project.title,
-      })
-    }
+    //   trackEventF(intercomEventEnum.DONATION_FINISHED, {
+    //     amount: artifactQuantity.toString(),
+    //     tokenId,
+    //   })
 
-    //Show error
-    if (error === 'Insufficient funds') {
-      //insufficientFunds
-      toggleModal('insufficientFunds')
-      setSending(false)
-      return
-    }
+    //   setVisibleModalWithAttrs('shareTransaction', {
+    //     mode: 'postTransaction',
+    //     destination: `/projects/${project.titleURL}`,
+    //     projecTitle: project.title,
+    //   })
+    // }
+
+    // //Show error
+    // if (error === 'Insufficient funds') {
+    //   //insufficientFunds
+    //   toggleModal('insufficientFunds')
+    //   setSending(false)
+    //   return
+    // }
 
     //Close modal if there is no error neither txHash, like when users have rejected transactions,
     toggleModal()
@@ -85,7 +112,7 @@ const DonationBox = ({ tokenId, project }: IDonationBox) => {
           </Cost>
           <Counter value={artifactQuantity} onChange={setArtifactQuantity} min={1} max={99} />
         </MobileBreak>
-        <StyledButton level={1} onClick={() => donateFn()} disabled={artifactQuantity <= 0 || sending}>
+        <StyledButton level={1} onClick={() => donateFn()} disabled={artifactQuantity <= 0 || sending || !write}>
           {sending ? 'Buying' : 'Buy'}
         </StyledButton>
       </>
