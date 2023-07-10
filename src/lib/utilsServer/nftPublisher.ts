d674f34a62cf06218297d9d25ba5f2bee148cee0
rubelux <rubelux@gmail.com>
2023-07-10 15:42:08 +0100

    added changes

--- src/lib/utilsServer/nftPublisher.ts
+++ src/lib/utilsServer/nftPublisher.ts
@@ -1,30 +1,69 @@
 import { assert } from '@lib'
-
 import pinataSDK from '@pinata/sdk'
-
+import axiosRetry from 'axios-retry'
+import { Readable } from 'stream'
+import axios from 'axios'
+import FormData from 'form-data'
+import fs from 'fs'
 /**
  * Reads an image file from `imageUrl` and stores an NFT with the given name.
  * @param {string} imageUrl the url to an image file
  * @param {string} name a name for the NFT
  */
 export async function storeNFTFromUrl(imageUrl: string, name: string) {
+  const formData = new FormData()
+  const axiosInstance = axios.create()
+
+  axiosRetry(axiosInstance, { retries: 5 })
+
   const pinataJWTKey = assert(assert(process.env.PINATA_JWT_KEY, 'PINATA_JWT_KEY'))
-  const pinata = new pinataSDK({
-    pinataJWTKey,
+
+  // const options = {
+  //   pinataMetadata: {
+  //     name,
+  //   },
+  // }
+
+  const response = await axiosInstance(imageUrl, {
+    method: 'GET',
+    responseType: 'stream',
+  })
+  formData.append(`file`, response.data)
+
+  formData.append('pinataMetadata', JSON.stringify({ name: 'test' }))
+
+  // const response = await fetch(imageUrl)
+
+  // console.log('response', response)
+
+  // const data = await response.json()
+
+  // console.log('data:::', data)
+
+  // const response = await axios.get<Readable>(imageUrl, {
+  //   responseType: 'stream',
+  // })
+
+  // // console.log('data in here ', data)
+
+  // const result = await pinata.pinFileToIPFS(response, options)
+
+  // console.log('storeNFTFromUrl result', result)
+  console.log('new:::::::::')
+  const res = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS ', formData, {
+    // maxBodyLength: 'Infinity',
+    maxContentLength: 100000000,
+    maxBodyLength: 1000000000,
+    headers: {
+      // @ts-ignore
+      'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+      Authorization: `Bearer ${pinataJWTKey}`,
+    },
   })
 
-  const options = {
-    pinataMetadata: {
-      name,
-    },
-  }
+  console.log('res::::::::', res.data)
 
-  const response = await fetch(imageUrl)
-  const result = await pinata.pinFileToIPFS(response.body, options)
-
-  console.log('storeNFTFromUrl result', result)
-
-  return result
+  return res.data
 }
 
 /**
