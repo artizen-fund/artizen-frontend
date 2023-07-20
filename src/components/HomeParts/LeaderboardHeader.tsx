992434c8a990c3c217022edfd5cd53be0360e716
EricWvgg <eric@whiskyvangoghgo.com>
2023-07-20 10:52:29 -0400

    copy edit

--- src/components/HomeParts/LeaderboardHeader.tsx
+++ src/components/HomeParts/LeaderboardHeader.tsx
@@ -25,7 +25,7 @@ const LeaderboardHeader = ({
 
         <Stats>
           <Stat>
-            <Label>Match Funds</Label>
+            <Label>{seasonIsActive ? 'Match Funds' : 'Funds Awarded'}</Label>
             {!!loading && <Data>…</Data>}
             {!loading && <Data>{totalPrizePooled} ETH</Data>}
             {/* {!loading && (
