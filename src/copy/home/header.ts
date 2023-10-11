370b1dbb95264af3dfcfb5cbea436e0205964445
rubelux <rubelux@gmail.com>
2023-10-11 16:10:13 +0100

    added last code

--- src/copy/home/header.ts
+++ src/copy/home/header.ts
@@ -1,5 +1,5 @@
 const header = {
-  title: 'Buy Artifacts to Fund Creativity',
+  title: 'Buy Artifacts to Fund Human Creativity',
   subtitle:
     'Buy cultural Artifacts to unlock match funding for projects at the frontier of art, science, technology, and design.',
   buttonLabel: 'See Artifacts',
