2d21bcad20ee5d86b710710667b6f6593e0f3431
rubelux <rubelux@gmail.com>
2023-10-11 15:41:36 +0100

    added last code

--- src/components/HomeParts/PartnersRibbon.tsx
+++ src/components/HomeParts/PartnersRibbon.tsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 import { assetPath } from '@lib'
 import { breakpoint, typography } from '@theme'
 import { partners as copy } from '@copy/home'
