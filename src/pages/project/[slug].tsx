bae381ad293a189c162582d0842c106deba1f7b6
rubelux <rubelux@gmail.com>
2023-10-12 14:37:33 +0100

    added last code

--- src/pages/project/[slug].tsx
+++ src/pages/project/[slug].tsx
@@ -21,11 +21,28 @@ import { typography, breakpoint } from '@theme'
 import { useQuery, useSubscription } from '@apollo/client'
 import { GET_PROJECTS, SUBSCRIBE_OPEN_EDITIONS, LOAD_OPEN_EDITIONS } from '@gql'
 import { IProjectFragment, IOpenEditionsSubscription, ISubmissionFragment } from '@types'
+import Projects from '../admin'
 
-const ProjectPage = ({ project }: any) => {
-  if (!project) {
-    return null
-  }
+const ProjectPage = () => {
+  const {
+    asPath,
+    query: { slug },
+  } = useRouter()
+
+  // console.log('project  ', project)
+
+  const { data, loading } = useQuery(GET_PROJECTS, {
+    skip: !slug,
+    variables: {
+      where: {
+        titleURL: {
+          _eq: slug,
+        },
+      },
+    },
+  })
+
+  const project: any = data?.Projects?.length > 0 && data?.Projects[0]
 
   console.log('project  ', project)
 
@@ -37,33 +54,28 @@ const ProjectPage = ({ project }: any) => {
   } = useContext(SeasonSubcriptionContext)
   const { setVisibleModalWithAttrs } = useContext(LayoutContext)
 
-  console.log('seasonData   ', seasonData)
-  console.log('loadingSeason  ', loadingSeason)
-
   //this should be only done when the season is active otherwise we should use the season from the project
   const { data: openEditionsSub } = useSubscription<IOpenEditionsSubscription>(SUBSCRIBE_OPEN_EDITIONS, {
-    skip: !seasonIsActive,
+    skip: !project || !seasonIsActive,
     fetchPolicy: 'no-cache',
     variables: {
       where: {
-        artifactId: { _eq: project?.artifacts[0].id },
+        artifactId: { _eq: project && project.artifacts[0].id },
       },
     },
   })
 
   const { data: openEditionsQuery } = useQuery(LOAD_OPEN_EDITIONS, {
-    skip: seasonIsActive,
+    skip: !project || seasonIsActive,
     fetchPolicy: 'no-cache',
     variables: {
       where: {
-        artifactId: { _eq: project?.artifacts[0].id },
+        artifactId: { _eq: project && project?.artifacts[0].id },
       },
     },
   })
 
   const openEditions = openEditionsSub || openEditionsQuery
-
-  const { asPath } = useRouter()
 
   const lead = project?.members?.find((m: any) => m.type === 'lead')?.user
 
@@ -81,111 +93,115 @@ const ProjectPage = ({ project }: any) => {
   return (
     <Layout>
       <PagePadding>
-        <Wrapper>
-          <Side>
-            <Header>
-              {loadingSeason && <ProjectDescriptionShimmer />}
-              <>
-                <Topline>
-                  <div>
-                    {!loadingSeason && (
-                      <RankAndArtifactCount
-                        rank={rank}
-                        count={count}
-                        seasonIsActive={seasonIsActive}
-                        totalSales={totalSales ? totalSales : 0}
-                        matchFundPooled={seasonData?.matchFundPooled}
-                      />
-                    )}
-                  </div>
-
-                  <Button
-                    level={2}
-                    outline
-                    onClick={() =>
-                      setVisibleModalWithAttrs('share', {
-                        mode: 'project',
-                        destination: asPath,
-                        projectTitle: titleCase(project.title),
-                        artizenHandle: lead?.artizenHandle,
-                        twitterHandle: getTwitterHandler(lead?.twitterHandle),
-                      })
-                    }
-                  >
-                    Share
-                  </Button>
-                </Topline>
-                <h1>{project.title}</h1>
-                <p>{project.logline}</p>
-                <Tags tags={project.impactTags?.split(',') || []} />
-
-                {lead && <CreatorBox user={lead} />}
-              </>
-            </Header>
-
-            {loadingSeason && <ProjectLeaderboardShimmer />}
-            {openEditions && seasonData && (
-              <>
-                <Leaderboard
-                  openEditions={openEditions}
-                  isWinner={rank === 0}
-                  count={count}
-                  totalSales={totalSales ? totalSales : 0}
-                  matchFundPooled={seasonData?.matchFundPooled}
-                />
-                <ProjectSponsors projectId={project.id} />
-              </>
-            )}
-            {project?.metadata && project?.metadata.length > 0 && (
-              <LongDescription>
-                {(project.metadata as Array<{ title: string; value: string }>).map((metadatum, index) => (
-                  <div key={`metadatum-${index}`}>
-                    <h2>{metadatum.title}</h2>
-                    <p>{metadatum.value}</p>
-                  </div>
-                ))}
-              </LongDescription>
-            )}
-          </Side>
-          {project.artifacts && project.artifacts.length > 0 && (
+        {loading && <ProjectDescriptionShimmer />}
+        {project && (
+          <Wrapper>
             <Side>
-              <ArtifactCard
-                count={count}
-                artifact={project.artifacts[0]}
-                project={project}
-                seasonIsActive={seasonIsActive || false}
-              />
+              <Header>
+                {loadingSeason && <ProjectDescriptionShimmer />}
+                <>
+                  <Topline>
+                    <div>
+                      {!loadingSeason && (
+                        <RankAndArtifactCount
+                          rank={rank}
+                          count={count}
+                          seasonIsActive={seasonIsActive}
+                          totalSales={totalSales ? totalSales : 0}
+                          matchFundPooled={seasonData?.matchFundPooled}
+                        />
+                      )}
+                    </div>
+
+                    <Button
+                      level={2}
+                      outline
+                      onClick={() =>
+                        setVisibleModalWithAttrs('share', {
+                          mode: 'project',
+                          destination: asPath,
+                          projectTitle: titleCase(project.title),
+                          artizenHandle: lead?.artizenHandle,
+                          twitterHandle: getTwitterHandler(lead?.twitterHandle),
+                        })
+                      }
+                    >
+                      Share
+                    </Button>
+                  </Topline>
+                  <>
+                    <h1>{project.title}</h1>
+                    <p>{project.logline}</p>
+                    <Tags tags={project.impactTags?.split(',') || []} />
+                    {lead && <CreatorBox user={lead} />}
+                  </>
+                </>
+              </Header>
+
+              {loadingSeason && <ProjectLeaderboardShimmer />}
+              {openEditions && seasonData && (
+                <>
+                  <Leaderboard
+                    openEditions={openEditions}
+                    isWinner={rank === 0}
+                    count={count}
+                    totalSales={totalSales ? totalSales : 0}
+                    matchFundPooled={seasonData?.matchFundPooled}
+                  />
+                  <ProjectSponsors projectId={project.id} />
+                </>
+              )}
+              {project?.metadata && project?.metadata.length > 0 && (
+                <LongDescription>
+                  {(project.metadata as Array<{ title: string; value: string }>).map((metadatum, index) => (
+                    <div key={`metadatum-${index}`}>
+                      <h2>{metadatum.title}</h2>
+                      <p>{metadatum.value}</p>
+                    </div>
+                  ))}
+                </LongDescription>
+              )}
             </Side>
-          )}
-        </Wrapper>
+            {project.artifacts && project.artifacts.length > 0 && (
+              <Side>
+                <ArtifactCard
+                  count={count}
+                  artifact={project.artifacts[0]}
+                  project={project}
+                  seasonIsActive={seasonIsActive || false}
+                />
+              </Side>
+            )}
+          </Wrapper>
+        )}
       </PagePadding>
     </Layout>
   )
 }
 
-export const getServerSideProps = async (context: any) => {
-  const { slug } = context.params
+// export const getServerSideProps = async (context: any) => {
+//   const { slug } = context.params
 
-  console.log('slug     ', slug)
+//   console.log('slug     ', slug)
 
-  const apolloClient = createApolloClient()
-  const projects = await apolloClient.query({
-    query: GET_PROJECTS,
-    variables: {
-      where: {
-        titleURL: {
-          _eq: slug,
-        },
-      },
-    },
-  })
+//   const apolloClient = createApolloClient()
+//   const projects = await apolloClient.query({
+//     query: GET_PROJECTS,
+//     variables: {
+//       where: {
+//         titleURL: {
+//           _eq: slug,
+//         },
+//       },
+//     },
+//   })
 
-  return {
-    props: {
-      project: projects.data.Projects[0],
-    },
-  }
-}
+//   return {
+//     props: {
+//       project: projects.data.Projects[0],
+//     },
+//   }
+// }
 
 // export async function getStaticPaths() {
 //   const apolloClient = createApolloClient()
