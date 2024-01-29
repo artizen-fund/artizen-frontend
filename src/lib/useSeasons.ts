13f1fdae2e0aeb2202d93324e8a60e192fce9a9c
Ruben Campos de Teba <rubencamposdeteba@Rubens-MacBook-Air.local>
2024-01-29 17:25:15 +0000

    yes

--- src/lib/useSeasons.ts
+++ src/lib/useSeasons.ts
@@ -113,8 +113,6 @@ export const useSeasons = () => {
     // return
   }
 
-  
-
   const closeSeason = async (seasonIndex: number) => {
     // console.log('season ID', typeof seasonIndex, seasonIndex)
     // const tx = await seasonsContract?.closeSeason(seasonIndex)
